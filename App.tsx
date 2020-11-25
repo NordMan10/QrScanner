a565a76c456639889c4742997d345376e15af37e
NordMan10 <yury.ten66@yandex.ru>
2020-11-25 13:31:35 +0500

    Start commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking } from "react-native";
+import { useCallback } from "react";
+import { useState, useEffect } from 'react'
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function App() {
+const App = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [supUrl, setUrl] = useState("https://github.com/NordMan10/battleships")
+
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(supUrl);
+
+    if (supported) {
+      await Linking.openURL(supUrl);  
+    } else {
+      alert(`Don't know how to open this link: ${supUrl}`);
+    }
+  }, [supUrl]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setUrl(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View
+          style={styles.scanner}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+
+          {scanned && <Button title={'Let\'s go!'} onPress={() =>
+            { 
+              handlePress();
+            }}/>}
+          
+          {scanned && <Button title={'Tap to Scan Again'} onPress={() =>
+            { 
+              setScanned(false);
+            }} />}
+        </View>
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1,},
+  scanner: 
+  {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  }
 });
+
+export default App;
